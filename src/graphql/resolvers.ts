af06d96a567c57dc03456c80fcac7e01ca421dd5
kevnflyn <rwestanton@gmail.com>
2022-09-12 14:02:04 +0200

    feat: add graph model for cars data

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,35 @@
+import { Company } from "@models/Company"
+import { Brand } from "@models/Brand"
+import { Car } from "@models/Car"
+
+interface GetCarInput {
+  id: number
+}
+
 // The root provides a resolver function for each API endpoint
 export const graphqlResolvers = {
-  hello: () => {
-    return 'Hello world!'
+  getCar: async ({ id }: GetCarInput) => {
+    if (isNaN(id)) {
+      const error: CustomGraphQLError = new Error('Bad request')
+      error.status = 400
+      throw error
+    }
+
+    const car = await Car.fetchById(id)
+    if (!car) {
+      const error: CustomGraphQLError = new Error('Car not found')
+      error.status = 404
+      throw error
+    }
+
+    const brand = await Brand.fetchById(car.brandId)
+
+    const company = brand ? await Company.fetchById(brand.companyId) : null
+
+    return {
+      ...car,
+      brand,
+      company
+    }
   }
 }
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,7 +4,30 @@ import { buildSchema } from 'graphql'
  * Construct a schema, using GraphQL schema language
  */
 export const graphqlSchema = buildSchema(`
- type Query {
-   hello: String
- }
+  type Brand {
+    id: ID!
+    name: String!
+    companyId: ID!
+  }
+
+  type Company {
+    id: ID!
+    name: String!
+  }
+
+  type Car {
+    id: ID!
+    model: String!
+    brandid: ID!
+    brand: Brand!
+    company: Company!
+  }
+
+  type RootQuery {
+    getCar(id: ID!): Car!
+  }
+
+  schema {
+    query: RootQuery
+  }
 `)
